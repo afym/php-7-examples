9c894c086a73d73f5b8b558d77572ccda19e237c
Francisco Ybarhuen <angel.fym@gmail.com>
2016-06-25 09:27:57 -0500

    Demo php 7 1+

---
+++ uniform-sintax/02.php
@@ -0,0 +1,6 @@
+<?php
+
+// Accediendo a un atributo desde un valor de un array PHP 5.6
+$obj->{$properties['name']}
+// Accediendo a un atributo desde un valor de un array PHP 5.7
+{$obj->$properties}['name']
